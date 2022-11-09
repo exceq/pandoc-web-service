7df42a9559500a02369a9f968bc24caf0af808de
exceq <mihail.petuxov@yandex.ru>
2022-11-09 19:42:10 +0500

    Сохранение пдф в minio

--- file-generator/app/main.py
+++ file-generator/app/main.py
@@ -2,8 +2,11 @@ import time
 
 import pypandoc as pd
 from fastapi import FastAPI
+from minio.helpers import ObjectWriteResult
 from pydantic import BaseModel
-from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
+
+from .storage_connect import *
 
 
 class Item(BaseModel):
@@ -11,6 +14,7 @@ class Item(BaseModel):
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/samples/static"), name="static")
 
 default_css_path: str = 'app/static/resume.css'
 default_header_path: str = 'app/samples/templates/header.html'
@@ -21,10 +25,14 @@ async def root():
     return {"message": "Hello World from file-generator"}
 
 
-@app.post("/pdf", response_class=FileResponse)
-async def generate_pdf(item: Item):
+@app.post("/pdf-to-minio")
+async def generate_pdf1(item: Item):
     html = pd.convert_text(source=item.markdown, format='markdown', to='html',
                            extra_args=['-s', '--section-divs', '-H', default_header_path, '--css', default_css_path])
-    filename = f'/app/out/pdf-{time.time()}.pdf'
-    text = pd.convert_text(source=html, format='html', to='pdf', outputfile=filename)
-    return FileResponse(filename, media_type='application/octet-stream', filename='file.pdf')
+    filename = f'pdf-{time.time()}.pdf'
+    filepath = f'./out/' + filename
+    pd.convert_text(source=html, format='html', to='pdf', outputfile=filepath)
+    # todo put request with bucket_name, filename
+    file: ObjectWriteResult = put_file("pdf", filename, filepath)
+    os.remove(filepath)
+    return {"bucket": file.bucket_name, "filename": file.object_name}
---
+++ file-generator/app/storage_connect.py
@@ -0,0 +1,44 @@
+import os
+
+from minio import Minio
+
+# Create a client with the MinIO server playground, its access key
+# and secret key.
+client = Minio(  # todo to env
+    "storage:9000",
+    access_key="minio",
+    secret_key="minio124",
+    secure=False
+)
+
+
+def put_file(bucket_name: str, object_name: str, file_path: str):
+    """
+    Uploads data from a file to an object in a bucket.
+
+    :param bucket_name: Name of the bucket.
+    :param object_name: Object name in the bucket.
+    :param file_path: Name of file to upload.
+    :return: :class:`ObjectWriteResult` object.
+    """
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError()
+
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+
+    return client.fput_object(bucket_name, object_name, file_path)
+
+
+def put_object(bucket_name: str, object_name: str, data):
+    """
+    Uploads data from a file to an object in a bucket.
+
+    :param bucket_name: Name of the bucket.
+    :param object_name: Object name in the bucket.
+    :param data: Data to uplaod.
+    :return: :class:`ObjectWriteResult` object.
+    """
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+    return client.put_object(bucket_name, object_name, data, len(data.data))
